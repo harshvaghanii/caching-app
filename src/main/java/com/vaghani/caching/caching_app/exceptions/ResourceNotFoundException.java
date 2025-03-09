0d9bd719b1cf45f656adf0496605b154a408e0a5
harshvaghanii <harshvaghani98@gmail.com>
2025-03-09 12:35:50 -0500

    adding exception files

---
+++ src/main/java/com/vaghani/caching/caching_app/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.vaghani.caching.caching_app.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
